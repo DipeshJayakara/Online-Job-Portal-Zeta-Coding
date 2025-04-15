f3f7eb34e534580a69d6d97d5c5686de5b363dcd
Dipesh Jayakara Poojary <142597315+DipeshJayakara@users.noreply.github.com>
2025-04-15 16:19:45 +0530

    Delete admin directory

--- admin/admin-auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Redirect if admin is not logged in
-if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
-    header('Location: admin-login.php');
-    exit();
-}
-?>
